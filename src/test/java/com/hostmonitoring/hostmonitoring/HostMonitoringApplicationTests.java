b67d4bc45b2835520085dfc712d930a9db54e1bd
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2024-10-22 19:34:17 +0200

    Initial commit

---
+++ src/test/java/com/hostmonitoring/hostmonitoring/HostMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostmonitoring.hostmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
