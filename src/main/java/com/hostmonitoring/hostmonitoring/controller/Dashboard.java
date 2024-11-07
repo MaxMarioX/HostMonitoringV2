b1c2c00331e080501b1eebe65f86b52fea1a9eec
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2024-11-07 19:27:48 +0100

    Done.

--- src/main/java/com/hostmonitoring/hostmonitoring/controller/Dashboard.java
+++ src/main/java/com/hostmonitoring/hostmonitoring/controller/Dashboard.java
@@ -1,20 +1,11 @@
 package com.hostmonitoring.hostmonitoring.controller;
-
-import com.hostmonitoring.hostmonitoring.entity.Host;
-import com.hostmonitoring.hostmonitoring.repository.HostRepository;
-import com.hostmonitoring.hostmonitoring.service.Ping.Ping;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
 
 @Controller
 @AllArgsConstructor
 public class Dashboard {
-
-    private HostRepository hostRepository;
 
     @GetMapping("/dashboard")
     public String dashboard() {
